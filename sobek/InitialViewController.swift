e9a32ffb43fa3796a33dc7f617536b110778ce21
Lucas Barros de Assis <lbassis@inf.ufrgs.br>
2017-05-07 14:28:53 -0300

    Alamofire integration

--- sobek/InitialViewController.swift
+++ sobek/InitialViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class InitialViewController: UIViewController {
 	
@@ -25,25 +26,8 @@ class InitialViewController: UIViewController {
 		//let postString: [String:String] = ["TODO":"true", "ADJ":"true", "TOHAVE":"true", "VERBS":"true", "ADV":"true", "NOUNS":"true", "AVGCON":"", "INTER":"true", "LANG":"2", "PRON":"true", "MOB_ID":"17356698", "URL":"sobek.ufrgs.br", "TOBE":"true", "FREQ":"", "THES":"false"]
 		//let postString = "TODO=true, ADJ=true, TOHAVE=true, VERBS=true, ADV=true, NOUNS=true, AVGCON=, INTER=true, LANG=2, PRON=true, MOB_ID=73168828, URL=ufrgs.br, TOBE=true, FREQ=, THES=false"
 		
-		request.httpMethod = "POST"
-		request.httpBody = postString.data(using: .utf8)!
-		
-		let task = URLSession.shared.dataTask(with: request) { data, response, error in
-			guard let data = data, error == nil else {                                                 // check for fundamental networking error
-				print("error=\(error)")
-				return
-			}
-			
-			if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
-				print("statusCode should be 200, but is \(httpStatus.statusCode)")
-				print("response = \(response)")
-			}
-			
-			let responseString = String(data: data, encoding: .utf8)
-			print("responseString = \(responseString!)")
-			print("data = \(data)")
-		}
-		task.resume()
+		Alamofire.request(serverUrl, parameters: postString, encoding: URLEncoding.default)
+
 
 		
 		self.performSegue(withIdentifier: "segue", sender: 17356698)
